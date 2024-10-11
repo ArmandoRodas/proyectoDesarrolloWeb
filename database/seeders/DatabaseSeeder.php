95bc5622d5a84d58cc920d0d25a92f5e5522bb63
EdgarJ06 <erodasc9@miumg.edu.gt>
2024-10-10 23:30:18 -0600

    edgrodas/moduloProducto: Se crea Factory y Seeder para manipular datos de prueba en la base de datos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         Cliente::factory(2000)->create();
 
+        $this->call(ProductoSeeder::class);
+
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
